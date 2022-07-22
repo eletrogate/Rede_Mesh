72925b1b9019dfa8dede8a6325e4e45abccf2562
192Samu <samuel@eletrogate.com>
2022-07-22 09:45:39 -0300

    Initial commit

---
+++ Rede_Mesh_Basica.ino
@@ -0,0 +1,55 @@
+// este é um exemplo simples que usa a biblioteca PainlessMesh
+#include "painlessMesh.h"
+#define   MESH_PREFIX     "Nome_da_sua_rede" // aqui coloque o nome da sua rede
+#define   MESH_PASSWORD   "Senha" // Senha da sua rede Wi-Fi
+#define   MESH_PORT       5555
+
+Scheduler userScheduler;
+painlessMesh  mesh;
+
+void sendMessage() ; 
+
+Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+
+void sendMessage() {
+
+  String msg = "Hello from node ";
+  msg += mesh.getNodeId();
+  mesh.sendBroadcast( msg );
+  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+}
+
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE );
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
+}
+void loop() {
+  mesh.update();
+}
